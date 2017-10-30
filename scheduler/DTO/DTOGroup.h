95d0ee56c172e7aa0562fe8a91856d4172360e73
DESKTOP-T0QJIRJ\danus <https://github.com/danahrechyn123/Site.git>
2017-10-30 19:54:44 +0200

    Commenting code.(DTOGroup.h , Group.h)

--- scheduler/DTO/DTOGroup.h
+++ scheduler/DTO/DTOGroup.h
@@ -3,15 +3,35 @@
 #include <direct.h>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 class DTO_Group
 {
 public:
+
+	//info:
+	//
 	void save_new_group_date(Group obj);
+
+	//info:
+	//Create new group.
 	void create_new_group(Group obj);
+	
+	//info:
+	//Check if group.txt is valid.
 	bool is_group(string id);
+	
+	//info:
+	//TODO: Dana: Can we rename it ? ("find_group_by_id")
+	//Get id and find group with the same id.
 	Group find_date_group(string id);
+	
+	//info:
+	//
 	vector<Group> all_group_date();
+	
+	//info:
+	//Get id and delete group with the same id.
 	void delete_group(string id);
 };
--- scheduler/Entity/Group.h
+++ scheduler/Entity/Group.h
@@ -9,11 +9,20 @@ class Group {
 	string id;
 	string name; //(ÏÌ²-11)
 public:
+
+	//info:
+	//Constructors.
 	Group();
 	Group(string n, string i);
 	Group(const Group& obj);
+
+	//info:
+	//Setters.
 	void set_name(string);
-	string get_name();
 	void set_id(string);
+
+	//info:
+	//Getters.
+	string get_name();
 	string get_id();
 };
